c614e90401c5fb3b398ec3eada34615cbe56a2bc
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2023-04-03 22:01:35 +0600

    Update 977.cpp

--- 977.cpp
+++ 977.cpp
@@ -10,3 +10,32 @@ public:
     }
 };
 ///or it can be used like two pointer
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        if(nums.size() == 0)
+            return nums;
+
+        int s = 0, e = nums.size() - 1;
+        vector<int> res;
+        while(s <= e)
+        {
+            int leftVal = nums[s]*nums[s];
+            int rightVal = nums[e]*nums[e];
+
+            if(leftVal > rightVal)
+            {
+                res.push_back(leftVal);
+                s++;
+            }
+            else
+            {
+                res.push_back(rightVal);
+                e--;
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
