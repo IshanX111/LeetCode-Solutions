f3ba56bf8001da58c6ed94656e67ff192dcd15b8
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2023-04-06 01:30:13 +0600

    Add files via upload

---
+++ 46.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> >vv;
+        sort(nums.begin(),nums.end());
+        do{
+            vector<int>v;
+            for(int i=0;i<nums.size();i++){
+                v.push_back(nums[i]);
+            }
+            vv.push_back(v);
+        }while(next_permutation(nums.begin(),nums.end()));
+        return vv;
+    }
+};
